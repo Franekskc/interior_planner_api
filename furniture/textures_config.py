c8dbc320fbfec91766c9221377fa59d29086946a
Franekskc <franek5432@gmail.com>
2025-04-24 21:08:15 +0200

    more textures ids

--- furniture/textures_config.py
+++ furniture/textures_config.py
@@ -1,8 +1,27 @@
 WALL_TEXTURE_IDS = {
-    "Plaster001",
+    "Concrete046", 
+    "Bricks059", 
+    "Plaster003", 
+    "Bricks054", 
+    "Bamboo002C",
+    "WoodSiding004", 
+    "Wallpaper001C", 
+    "WoodSiding009", 
+    "Bricks058", 
+    "Bamboo001C"
+
 }
 
 FLOOR_TEXTURE_IDS = {
-    "PavingStones147",
-    "WoodFloor049",
+    "WoodFloor064", 
+    "Terrazzo019S", 
+    "WoodFloor062", 
+    "WoodFloor043", 
+    "PavingStones128",
+    "WoodFloor041", 
+    "Tiles131", 
+    "WoodFloor052", 
+    "PavingStones125A", 
+    "Tiles076", 
+    "WoodFloor025"
 }
